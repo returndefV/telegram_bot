f8824819901229d9b55956f4ed86e01ffd430ae3
Vlad <156532830+returndefV@users.noreply.github.com>
2024-07-02 23:53:06 +0300

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -2,14 +2,12 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from database.models import Base
+from database.orm_query import orm_add_banner_description, orm_create_categories
 
-#from .env file:
-# DB_LITE=sqlite+aiosqlite:///my_base.db
-# DB_URL=postgresql+asyncpg://login:password@localhost:5432/db_name
+from common.texts_for_db import categories, description_for_info_pages
 
-# engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
-engine = create_async_engine(os.getenv('DB_URL'), echo=True)
+engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
@@ -17,6 +15,10 @@ session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_c
 async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)
 
 
 async def drop_db():
